0d8c60ddbd299d458d4db12f92b58a7e3d05c025
Yannic Brügger <yannic.bruegger@gmail.com>
2021-06-08 00:16:48 +0200

    Add server functionality

---
+++ server/main.js
@@ -0,0 +1,52 @@
+const http = require('http');
+const ws = require('ws');
+
+const wss = new ws.Server({noServer: true});
+const admins = [];
+const users = [];
+
+function accept(req, res) {
+  // all incoming requests must be websockets
+  if (!req.headers.upgrade || req.headers.upgrade.toLowerCase() != 'websocket') {
+    res.end();
+    return;
+  }
+
+  // can be Connection: keep-alive, Upgrade
+  if (!req.headers.connection.match(/\bupgrade\b/i)) {
+    res.end();
+    return;
+  }
+
+  wss.handleUpgrade(req, req.socket, Buffer.alloc(0), onConnect);
+}
+
+function onConnect(ws) {
+  ws.on('message', function (message) {
+    let command;
+    try {
+      command = JSON.parse(message);
+      console.log(command);
+    } catch {
+      admin.send(JSON.stringify({message: 'ERROR'}));
+      return;
+    }
+
+    if(command.type === 'HELLO'){
+      if(command.role === 'admin'){
+        admins.push(ws);
+      }
+      if(command.role === 'user'){
+        users.push(ws);
+      }
+    }
+
+    if(command.type === 'RAISE'){
+      admins.forEach((admin) => {
+        admin.send(JSON.stringify({message: 'HANDRAISED'}));
+      })
+    }
+  });
+}
+
+http.createServer(accept).listen(8080);
