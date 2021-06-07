96399ee0335b598f30a91d6154bf0e0f1d5132dd
Yannic Brügger <yannic.bruegger@gmail.com>
2021-06-08 00:17:17 +0200

    Add user view

---
+++ client/main.js
@@ -0,0 +1,29 @@
+let socket = new WebSocket("ws://127.0.0.1:8080");
+
+socket.onopen = function(e) {
+  const data = {type: 'HELLO', role: 'user'};
+  socket.send(JSON.stringify(data));
+};
+
+socket.onmessage = function(event) {
+  console.log(`[message] Data received from server: ${event.data}`);
+};
+
+socket.onclose = function(event) {
+  if (event.wasClean) {
+    console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+  } else {
+    console.log('[close] Connection died');
+  }
+};
+
+socket.onerror = function(error) {
+  console.error(`[error] ${error.message}`);
+};
+
+function raiseHand() {
+  const data = {type: 'RAISE'}
+  socket.send(JSON.stringify(data));
+}
+
+document.querySelector('#✋').addEventListener('click', () => {raiseHand();})
