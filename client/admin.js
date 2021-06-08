9e5994a41349981765ada4434414b454d452301c
Yannic Brügger <yannic.bruegger@gmail.com>
2021-06-08 11:53:10 +0200

    Add reconnect

--- client/admin.js
+++ client/admin.js
@@ -1,47 +1,53 @@
-let socket = new WebSocket("wss://bcbb1475eec9.ngrok.io");
 let counter = 0;
+let socket;
 
-socket.onopen = function(e) {
-  const data = {type: 'HELLO', role: 'admin'};
-  socket.send(JSON.stringify(data));
-};
+function connect(){
+  socket = new WebSocket("wss://bcbb1475eec9.ngrok.io");
 
-socket.onmessage = function(event) {
-  let command;
-  try {
-    command = JSON.parse(event.data);
-    console.log(command);
-  } catch {
-    admin.send(JSON.stringify({message: 'ERROR'}));
-    return;
+  socket.onopen = function(e) {
+    const data = {type: 'HELLO', role: 'admin'};
+    socket.send(JSON.stringify(data));
+  };
+
+  socket.onmessage = function(event) {
+    let command;
+    try {
+      command = JSON.parse(event.data);
+      console.log(command);
+    } catch {
+      admin.send(JSON.stringify({message: 'ERROR'}));
+      return;
+    }
+    if(command.message === 'HANDRAISED') {
+      counter++;
+      triggerAnimation()
+    }
+  };
+
+  socket.onclose = function(event) {
+    if (event.wasClean) {
+      console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+      setTimeout(connect, 1000);
+    } else {
+      console.log('[close] Connection died');
+    }
+  };
+
+  socket.onerror = function(error) {
+    console.error(`[error] ${error.message}`);
+  };
+
+  function triggerAnimation() {
+    document.querySelector('#✋').classList.add('active');
+    document.querySelector('.votes').innerHTML = counter;
   }
-  if(command.message === 'HANDRAISED') {
-    counter++;
-    triggerAnimation()
-  }
-};
 
-socket.onclose = function(event) {
-  if (event.wasClean) {
-    console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
-  } else {
-    console.log('[close] Connection died');
-  }
-};
-
-socket.onerror = function(error) {
-  console.error(`[error] ${error.message}`);
-};
-
-function triggerAnimation() {
-  document.querySelector('#✋').classList.add('active');
-  document.querySelector('.votes').innerHTML = counter;
 }
+connect();
 
 function clearHands() {
   counter = 0;
   document.querySelector('#✋').classList.remove('active');
   document.querySelector('.votes').innerHTML = counter;
 }
-
 document.querySelector('#✋').addEventListener('click', () => {clearHands();})
--- client/main.js
+++ client/main.js
@@ -1,30 +1,37 @@
-let socket = new WebSocket("wss://bcbb1475eec9.ngrok.io");
+let socket;
 
-socket.onopen = function(e) {
-  const data = {type: 'HELLO', role: 'user'};
-  socket.send(JSON.stringify(data));
-};
+function connect(){
+  socket = new WebSocket("wss://bcbb1475eec9.ngrok.io");
 
-socket.onmessage = function(event) {
-  console.log(`[message] Data received from server: ${event.data}`);
-};
+  socket.onopen = function(e) {
+    const data = {type: 'HELLO', role: 'user'};
+    socket.send(JSON.stringify(data));
+  };
 
-socket.onclose = function(event) {
-  if (event.wasClean) {
-    socket = new WebSocket("wss://bcbb1475eec9.ngrok.io");
-    console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
-  } else {
-    console.log('[close] Connection died');
-  }
-};
+  socket.onmessage = function(event) {
+    console.log(`[message] Data received from server: ${event.data}`);
+  };
 
-socket.onerror = function(error) {
-  console.error(`[error] ${error.message}`);
-};
+  socket.onclose = function(event) {
+    if (event.wasClean) {
+      setTimeout(connect, 1000);
+      console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+    } else {
+      console.log('[close] Connection died');
+    }
+  };
+
+  socket.onerror = function(error) {
+    console.error(`[error] ${error.message}`);
+  };
+
+}
 
 function raiseHand() {
   const data = {type: 'RAISE'}
   socket.send(JSON.stringify(data));
 }
+
+connect();
 
 document.querySelector('#✋').addEventListener('click', () => {raiseHand();})
