bc0f8cfe7d4a5dc603e672fe78c0e49b7703caaf
Yannic Brügger <yannic.bruegger@gmail.com>
2021-06-08 11:50:43 +0200

    Add reconnect

--- client/main.js
+++ client/main.js
@@ -1,30 +1,34 @@
-let socket = new WebSocket("wss://bcbb1475eec9.ngrok.io");
+function connect(){
+  let socket = new WebSocket("wss://bcbb1475eec9.ngrok.io");
 
-socket.onopen = function(e) {
-  const data = {type: 'HELLO', role: 'user'};
-  socket.send(JSON.stringify(data));
-};
+  socket.onopen = function(e) {
+    const data = {type: 'HELLO', role: 'user'};
+    socket.send(JSON.stringify(data));
+  };
 
-socket.onmessage = function(event) {
-  console.log(`[message] Data received from server: ${event.data}`);
-};
+  socket.onmessage = function(event) {
+    console.log(`[message] Data received from server: ${event.data}`);
+  };
 
-socket.onclose = function(event) {
-  if (event.wasClean) {
-    socket = new WebSocket("wss://bcbb1475eec9.ngrok.io");
-    console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
-  } else {
-    console.log('[close] Connection died');
+  socket.onclose = function(event) {
+    if (event.wasClean) {
+      setTimeout(connect, 1000);
+      console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+    } else {
+      console.log('[close] Connection died');
+    }
+  };
+
+  socket.onerror = function(error) {
+    console.error(`[error] ${error.message}`);
+  };
+
+  function raiseHand() {
+    const data = {type: 'RAISE'}
+    socket.send(JSON.stringify(data));
   }
-};
-
-socket.onerror = function(error) {
-  console.error(`[error] ${error.message}`);
-};
-
-function raiseHand() {
-  const data = {type: 'RAISE'}
-  socket.send(JSON.stringify(data));
 }
+
+connect()
 
 document.querySelector('#✋').addEventListener('click', () => {raiseHand();})
